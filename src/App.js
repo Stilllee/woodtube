fb85c976db34e6b05489da0d3d6d1fccb68dbbec
Stilllee <orodae@gmail.com>
2024-02-17 19:05:33 +0900

    ✨ 비디오 카드 만들기 - Mock 데이터

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SearchHeader from "./Components/SearchHeader";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 export default function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
