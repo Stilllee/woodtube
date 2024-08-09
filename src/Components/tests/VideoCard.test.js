db7d17bdc9b9a5137cab4a8e9c5ef75f85477e48
Stilllee <orodae@gmail.com>
2024-08-10 01:24:57 +0900

    [test] 비디오 카드 테스트 - 동적

--- src/Components/tests/VideoCard.test.js
+++ src/Components/tests/VideoCard.test.js
@@ -1,29 +1,32 @@
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+
 import React from "react";
 import VideoCard from "../VideoCard";
 import { formatAgo } from "../../util/date";
 import { render } from "@testing-library/react";
 import { screen } from "@testing-library/dom";
+import { useLocation } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("VideoCard", () => {
-  it("비디오 항목을 렌더링", () => {
-    const video = {
-      id: 1,
-      snippet: {
-        title: "제목",
-        channelId: "1",
-        channelTitle: "채널명",
-        publishedAt: new Date(),
-        thumbnails: {
-          medium: {
-            url: "http://example.com/image.jpg",
-          },
+  const video = {
+    id: 1,
+    snippet: {
+      title: "제목",
+      channelId: "1",
+      channelTitle: "채널명",
+      publishedAt: new Date(),
+      thumbnails: {
+        medium: {
+          url: "http://example.com/image.jpg",
         },
       },
-    };
-    const { title, channelId, channelTitle, publishedAt, thumbnails } =
-      video.snippet;
+    },
+  };
+  const { title, channelId, channelTitle, publishedAt, thumbnails } =
+    video.snippet;
 
+  it("비디오 항목을 렌더링", () => {
     render(
       <MemoryRouter>
         <VideoCard video={video} />
@@ -36,5 +39,28 @@ describe("VideoCard", () => {
     expect(screen.getByText(title)).toBeInTheDocument();
     expect(screen.getByText(channelTitle)).toBeInTheDocument();
     expect(screen.getByText(formatAgo(publishedAt, "ko"))).toBeInTheDocument();
+  });
+
+  it("클릭하면 video state와 함께 비디오 상세 페이지로 이동", async () => {
+    function LocationStateDisplay() {
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<VideoCard video={video} />} />
+          <Route
+            path={`/videos/watch/${video.id}`}
+            element={<LocationStateDisplay />}
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const card = screen.getByRole("listitem");
+    await userEvent.click(card);
+
+    expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
   });
 });
