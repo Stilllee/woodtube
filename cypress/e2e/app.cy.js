04a0073b5fde1e67914b180bee2b1f81a10b3df5
Stilllee <orodae@gmail.com>
2024-08-18 21:20:44 +0900

    [test] 방문시 처음 렌더링되는 비디오 목록 테스트

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -3,10 +3,21 @@ import "@testing-library/cypress/add-commands";
 
 describe("Youtube App", () => {
   beforeEach(() => {
+    cy.intercept("GET", /(mostPopular)/g, {
+      fixture: "popular.json",
+    });
+    cy.intercept("GET", /(search)/g, {
+      fixture: "search.json",
+    });
+    cy.viewport(1200, 800);
     cy.visit("/");
   });
 
   it("렌더링", () => {
     cy.findByText("WoodTube").should("exist");
+  });
+
+  it("처음에는 popular 비디오 목록을 보여준다.", () => {
+    cy.findByText("혹시 포토샵 해본 적 있나?").should("exist");
   });
 });
